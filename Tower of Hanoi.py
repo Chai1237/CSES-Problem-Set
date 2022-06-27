c41db8e119b3b64bce5b379bb1fda89ff4929ed8
Chai1237 <hjie2003@gmail.com>
2022-06-28 03:21:25 +0800

    TowerOfHanoi

--- Tower of Hanoi.py
+++ Tower of Hanoi.py
@@ -3,5 +3,6 @@ def S(n, a,b,c):
         return a + c
     return S(n-1, a, c, b) + a + c + S(n-1, b, a, c)
 N = S(int(input()),"1","2","3")
+print(len(N)//2)
 for i in range(0,len(N),2):
     print(" ".join(N[i]+N[i+1]))
