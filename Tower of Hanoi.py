12f565b7fe7cd06d7e6998951844ae7d4ac3105b
Chai1237 <hjie2003@gmail.com>
2022-06-28 03:11:56 +0800

    TowerOfHanoi

--- Tower of Hanoi.py
+++ Tower of Hanoi.py
@@ -1,8 +1,7 @@
-n,s,e = int(input()),[],[]
-def F(x):
-    if x == 1:
-        s.append(1)
-        e.append(3)
-
-
-    
+def S(n, a,b,c):
+    if n == 1:
+        return a + c
+    return S(n-1, a, c, b) + a + c + S(n-1, b, a, c)
+N = S(int(input()),"1","2","3")
+for i in range(0,len(N),2):
+    print(" ".join(N[i]+N[i+1]))
