b14d01c26819b8c932891d3a681f4a79d7482b19
Chai1237 <hjie2003@gmail.com>
2022-07-26 22:34:45 +0800

    CQ

--- Chessboard and Queen.py
+++ Chessboard and Queen.py
@@ -1,9 +1,6 @@
-def RHV(x, y, r):
+def RH(x, y, r):
     for i in range(1, 9):
         n = [i, y]
-        if n in r:
-            r.remove(n)
-        n = [x, i]
         if n in r:
             r.remove(n)
     return r
@@ -27,13 +24,14 @@ def RD(x, y, r):
     return r
 
 def S(x, y, r):
+    r = list(r)
     n = [x, y]
     if n not in r:
         return 0
-    if x == 8:
+    elif x == 8:
         return 1
     else:
-        r = RHV(x, y, r)
+        r = RH(x, y, r)
         if y == 1:
             r = RU(x, y, r)
         elif y == 8:
@@ -44,11 +42,20 @@ def S(x, y, r):
     return S(x+1, 1, r) + S(x+1, 2, r) + S(x+1, 3, r) + S(x+1, 4, r) + S(x+1, 5, r) + S(x+1, 6, r) + S(x+1, 7, r) + S(x+1, 8, r)
 
 R, ans = [], 0
+for x in range(1, 9):
+    for y in range(1, 9):
+        R.append([x, y])
 
+for i in range(8,0,-1):
+    row = list(input())
+    if "*" in row:
+        ind = row.index("*")+1
+        R.remove([ind,i])
+        for q in range(row.count("*")-1):
+            ind = row.index("*",ind)+1
+            R.remove([ind,i])
+
+R = tuple(R)
 for y in range(1, 9):
-    for x in range(1, 9):
-        for y in range(1, 9):
-            R.append([x, y])
     ans += S(1, y, R)
-
 print(ans)
--- Testing.py
+++ Testing.py
@@ -3,33 +3,66 @@ a = [str(i) for i in range(1,n+1)]
 print(' '.join(a[1::2] + a[::2]))'''
 from collections import Counter
 from time import time
-from math import comb
+from math import comb, factorial
 from itertools import permutations, combinations
 # files = open('PR input.txt','r')
 start = time()
+R, reserve, ans = [], [], 0
+for x in range(1, 9):
+    for y in range(1, 9):
+        R.append([x, y])
 
-n = 0
-def QP(x,y, A):
-    A.append([x,y])
-    H, a, b = [],[],[]
-    if x != 8:
-        return QP(x+1, 1, A) + QP(x+1, 2, A) + QP(x+1, 3, A) + QP(x+1, 4, A) + QP(x+1, 5, A) + QP(x+1, 6, A) + QP(x+1, 7, A) + QP(x+1, 8, A)
-    else: 
-        for i in range(8):
-            n = A[i]
-            print(len(A))
-            H.append(n[1])
-            a.append(n[0]+n[1])
-            b.append(9-n[0]+n[1])
-        H, a, b = set(H), set(a), set(b)
-        if len(H) + len(a) + len(b) != 24:
-            return 0
-        else:
-            return 1
-for y in range(1,9):
-    n += QP(1, y, [])
+for i in range(8,0,-1):
+    row = list(input())
+    if "*" in row:
+        ind = row.index("*")+1
+        reserve.append([ind,i])
+        for q in range(row.count("*")-1):
+            ind = row.index("*",ind)+1
+            reserve.append([ind,i])
+            
+for i in reserve:
+    R.remove(i)
 
-print(n)
+print(tuple(R))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n = 0
+# def QP(x,y, A):
+#     A.append([x,y])
+#     H, a, b = [],[],[]
+#     if x != 8:
+#         return QP(x+1, 1, A) + QP(x+1, 2, A) + QP(x+1, 3, A) + QP(x+1, 4, A) + QP(x+1, 5, A) + QP(x+1, 6, A) + QP(x+1, 7, A) + QP(x+1, 8, A)
+#     else:
+#         return 1 
+#         for i in range(8):
+#             n = A[i]
+#             print(len(A))
+#             H.append(n[1])
+#             a.append(n[0]+n[1])
+#             b.append(9-n[0]+n[1])
+#         H, a, b = set(H), set(a), set(b)
+#         if len(H) + len(a) + len(b) != 24:
+#             return 0
+#         else:
+#             return 1
+# for y in range(1,9):
+#     n += QP(1, y, [])
+
+# print(n)
 
 
 
